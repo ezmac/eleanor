22e25aba6f66224a917c3434dd6198de501e5f87
David Pereira <inexhero@gmail.com>
2015-04-29 09:41:19 -0500

    only debian 8

--- install.sh
+++ install.sh
@@ -4,16 +4,11 @@ echo "INSTALLING ELEANOR PROJECT"
 echo "---------------------------"
 tput sgr0
 
-#Create temp folder
-tput setaf 4
-echo "*Creating temp folder"
-mkdir -p temp_eleanor
-tput sgr0
-
-
 tput setaf 4
 echo "*Downloading packages"
 tput sgr0
+su root <<'EOF'
+
 sudo apt-get install --no-install-recommends lua5.1 xmlto luadoc libxcb-randr0-dev libxcb-xtest0-dev \
  libxcb-xinerama0-dev  libxcb-shape0-dev libxcb-keysyms1-dev libxcb-icccm4-dev libx11-xcb-dev lua-lgi-dev \
  libstartup-notification0-dev libxdg-basedir-dev libxcb-image0-dev libxcb-util0-dev libgdk-pixbuf2.0-dev build-essential cmake graphicsmagick-imagemagick-compat libxcb-cursor0 cairo-clock libxcb-xkb1 libcairo2-dev libxcb-cursor-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xkb-dev liblua5.1-0 liblua5.1-0-dbg lua5.1 liblua5.1-0-dev curl graphicsmagick cmake curl xorg slim libasound2 alsa-utils alsa-oss alsa-tools-gui curl
@@ -24,6 +19,8 @@ tput setaf 4
 echo "*Downloading awesome wm"
 tput sgr0
 
+
+
 MACHINE_TYPE=`uname -m`
 if [${MACHINE_TYPE} == "x86_64"]
 then
@@ -32,14 +29,16 @@ then
     echo "*Installing awesome wm packages"
     tput sgr0
     sudo dpkg -i awesome_3.5.1-1_amd64.deb
+    apt-get install -f install
 else
     curl -O http://lmde-mirror.gwendallebihan.net/latest/pool/main/a/awesome/awesome_3.5.1-1_i386.deb
     tput setaf 4
     echo "*Installing awesome wm packages"
     tput sgr0
     sudo dpkg -i awesome_3.5.1-1_i386.deb
+    apt-get install -f install
 fi
-
+EOF
 #Installing themes
 tput setaf 4
 echo "*Installing themes"
